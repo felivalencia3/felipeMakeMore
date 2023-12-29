98494f8012495587d2926bd9e8c6b6a153cfe81c
Felipe Valencia <felipe.valencia.2003@gmail.com>
2023-12-29 10:53:57 -0500

    Using one-hot encoding

--- makemore.py
+++ makemore.py
@@ -31,3 +31,26 @@ for i in range(5):
             break
 
     print("".join(out))
+
+
+# Measure log likelihood
+log_likelihood = 0.0
+n = 0
+
+for w in words:
+#for w in ["andrejq"]:
+  chs = ['.'] + list(w) + ['.']
+  for ch1, ch2 in zip(chs, chs[1:]):
+    ix1 = stoi[ch1]
+    ix2 = stoi[ch2]
+    prob = P[ix1, ix2]
+    logprob = torch.log(prob)
+    log_likelihood += logprob
+    n += 1
+    #print(f'{ch1}{ch2}: {prob:.4f} {logprob:.4f}')
+
+# use negative log likelihood as loss
+nll = -log_likelihood
+# print(f'{nll=}')
+# want to minimize this:
+# print(f'{nll/n}')
