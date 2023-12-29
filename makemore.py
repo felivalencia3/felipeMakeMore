ee7cf002752103ff9af94ea59133b648c6406631
Felipe Valencia <felipe.valencia.2003@gmail.com>
2023-12-28 20:27:37 -0500

    Bigram Model - Version 1 - Very Bad

--- makemore.py
+++ makemore.py
@@ -1,5 +1,28 @@
-words = open("names.txt", "r").read().splitlines()
+words = open('names.txt', 'r').read().splitlines()
+import torch
+N = torch.zeros((27, 27), dtype=torch.int32)
+chars = sorted(list(set(''.join(words))))
+stoi = {s:i+1 for i,s in enumerate(chars)}
+stoi['.'] = 0
+itos = {i:s for s,i in stoi.items()}
+for w in words:
+  chs = ['.'] + list(w) + ['.']
+  for ch1, ch2 in zip(chs, chs[1:]):
+    ix1 = stoi[ch1]
+    ix2 = stoi[ch2]
+    N[ix1, ix2] += 1
 
-# Part 1: Bigram
-# Only look at two characters
-# Predict next immediate char based only on current character.
+g = torch.Generator().manual_seed(2147483647)
+for i in range(20):
+    ix = 0
+    out = []
+    while True:
+        p = N[ix].float()
+        p = p / p.sum()
+        ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
+        out.append(itos[ix])
+        # end token
+        if ix == 0:
+            break
+
+    print("".join(out))
